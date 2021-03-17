9a63fcead94550166dcb4cf4ee089d9777f64edd
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-16 20:32:48 -0500

    reworked how the cities are saved to the search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,69 @@
-var apiKey = "78d3a1bd5e04e59d4e8c0c5c026799bd"
-var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather`;
-var currentDate = moment().format("M/D/YY");
+$(document).ready(function() { 
+    
+    // document variables
+    var apiKey = "78d3a1bd5e04e59d4e8c0c5c026799bd"
+    var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather`;
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast`;
+    var currentDate = moment().format("M/D/YY");
+    var searchBtn = $("#search-form");
+    var historyContainer = $("#search-history");
 
-$("#search-button").click(function (event) {
-    event.preventDefault();
-    var citySearch = $("input[id='search-value']").val();
+    searchBtn.submit(function (event) {
+        event.preventDefault();
 
-    // adds error text if user does not put in a city
-    var errorText = $("<p class='text-danger ml-2' id='error'>Please enter a city!</p>");
-    var noEntry = $(".aside")
-    if (!citySearch) {
-        noEntry.append(errorText);
-        return;
-    }
+        var searchValues = $(this).serializeArray();
+        var city = searchValues[0].value;
+        var searchHistoryDiv = $("<div class='search-history m-2 bg-light col-md-3 rounded'>");
+        searchHistoryDiv.text(city);
+        historyContainer.append(searchHistoryDiv);
 
-    // appends the user's city to the search history and turns the searched city into a button
-    var userCity = $("<button class='prevCity col-md-5 m-1 btn btn-light'><li>");
+        console.log(searchValues, city);
 
-    userCity.text(citySearch);
+        searchForWeather(city);
 
-    var searchHistoryList = $(".history")
-    searchHistoryList.append(userCity);
-
-    // removes the error text
-    $("#error").remove();
-
-    // fetch request
-    var requestUrl = `${currentWeatherUrl}?q=${citySearch}&appid=${apiKey}&units=imperial`;
-    function currentWeatherApi() {
-        fetch(requestUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-                console.log(data.main.temp + " °F");
-                console.log(data.main.humidity + "%");
-                console.log(data.wind.speed + " MPH");
-
-                $(".city").remove();
-                var icon = (data.weather[0].icon);
-                var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-                var iconImg = "<img src ="+iconUrl+">";
-                var cityName = '<h1 class="city m-1">' + (data.name) + ' (' + currentDate + ')' + iconImg + '</h1>';
+        // fetch request
+        var requestUrl = `${currentWeatherUrl}?q=${city}&appid=${apiKey}&units=imperial`;
+        function currentWeatherApi() {
+            fetch(requestUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    $(".city").remove();
+                    var icon = (data.weather[0].icon);
+                    var iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                    var iconImg = "<img src ="+iconUrl+">";
                 
-                var forecastToday = $("#today").addClass("today");
-                forecastToday.append(cityName);
+                    var cityName = '<h1 class="city m-1">' + (data.name) + ' (' + currentDate + ')' + iconImg + '</h1>';
+                    
+                    var forecastToday = $("#today").addClass("today");
+                    forecastToday.append(cityName);
 
-                var temp = '<h5 class="m-1 pb-5 city">' + 'Temperature: ' + (data.main.temp) + '°F' + '</h5>';
-                var humidity = '<h5 class="m-1 pb-5 city">' + 'Humidity: ' + (data.main.humidity) + '%' + '</h5>';
-                var windSpeed = '<h5 class="m-1 pb-5 city">' + 'Wind Speed: ' + (data.wind.speed) + ' MPH' + '</h5>';
-                
-                forecastToday.append(temp);
-                forecastToday.append(humidity);
-                forecastToday.append(windSpeed);                       
-            })            
+                    var temp = '<h5 class="m-1 pb-5 city">' + 'Temperature: ' + (data.main.temp) + '°F' + '</h5>';
+                    var humidity = '<h5 class="m-1 pb-5 city">' + 'Humidity: ' + (data.main.humidity) + '%' + '</h5>';
+                    var windSpeed = '<h5 class="m-1 pb-5 city">' + 'Wind Speed: ' + (data.wind.speed) + ' MPH' + '</h5>';
+                    
+                    forecastToday.append(temp);
+                    forecastToday.append(humidity);
+                    forecastToday.append(windSpeed);                       
+                })            
+        }
+        currentWeatherApi();   
+        
+        var rqstUrl = `${forecastUrl}?q=${city}&cnt=5&appid=${apiKey}&units=imperial`;
+        function forecastApi() {
+            fetch(rqstUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
+        }
+        forecastApi();
+    })
+
+    function searchForWeather(city) {
+        console.log(city);
     }
-    currentWeatherApi();    
 })
