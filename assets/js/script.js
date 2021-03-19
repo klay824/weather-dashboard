423d0e07976e72a8c0ab94e16d7c7852aec3f3c7
katelyn chadwell <kchadwell0226@gmail.com>
2021-03-18 20:27:27 -0500

    playing around with limiting how many buttons are generating could not get to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,23 +18,23 @@ $(document).ready(function () {
         var city = searchValues[0].value;
         var searchHistoryDiv = $("<button type='button' class='btn search-history mt-2 mr-2 mb-2 p-2 text-center bg-light col-sm-1 col-md-4 col-lg-7 rounded'>");
 
+
         // adding click power to dynamically generated buttons
-        searchHistoryDiv.click(function(event) {
+        searchHistoryDiv.click(function (event) {
             event.preventDefault();
-            var value = $(this).text();
-            currentWeatherApi(value);
+            var value = $(this).text(); /* button clicks */
+            currentWeatherApi(value); /* adding the values from the two forecast api functions */
             forecastApi(value);
         });
 
+        
         // pushing city value to searchHistory array, then saving to local storage
         searchHistory.push(city);
+
         localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-        
+
         searchHistoryDiv.text(city);
         historyContainer.append(searchHistoryDiv);
-        // localStorage.setItem("search", city);
-
-        // searchForWeather(city);
 
         // today's weather fetch request
         var requestUrl = `${currentWeatherUrl}?q=${city}&appid=${apiKey}&units=imperial`;
@@ -77,7 +77,7 @@ $(document).ready(function () {
                     // looping through the 40 results since it gives weather for every three hours for 5 days and we only need 5 days at the same time
                     for (var i = 0; i < data.list.length; i++) {
                         var isThreeOClock = data.list[i].dt_txt.search('15:00:00'); /* searching for only 3pm */
-                        
+
                         var forecastContainer = $("#forecast");
                         if (isThreeOClock > -1) {
                             var forecast = data.list[i]
@@ -85,17 +85,17 @@ $(document).ready(function () {
                             var icon = (forecast.weather[0].icon);
                             var iconUrl = "http://openweathermap.org/img/wn/" + icon + ".png";
                             var iconImg = "<img src =" + iconUrl + ">";
-                            
+
                             var temp = forecast.main.temp;
                             var humidity = forecast.main.humidity;
-                            
+
                             var rowDiv = $("<div class='col-sm-3 col-md-4 col-lg-2 forecastBox mt-1 mr-3 mb-1 justify-content-center remove'>")
                             var dayDiv = $("<div class='day-name text-center remove pt-2'>");
                             var iconDiv = $("<div class='icon-name text-center remove'>" + iconImg + "</div");
                             var tempDiv = $("<div class='temp-name text-center remove'>");
                             var humidityDiv = $("<div class='humidity-name text-center remove pb-2'>");
-                            
-                            
+
+
                             dayDiv.text(day);
                             tempDiv.text("Temp: " + temp + " °F");
                             humidityDiv.text("Humidity: " + humidity + "%");
@@ -116,10 +116,11 @@ $(document).ready(function () {
     function retrieveHistory() {
         if (localStorage.getItem("searchHistory")) {
             searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-            for (var i=0; i < searchHistory.length; i++) {
+            
+            for (var i = 0; i < searchHistory.length; i++) {
                 var searchHistoryDiv = $("<button type='button' class='btn search-history mt-2 mr-2 mb-2 p-2 text-center bg-light col-sm-1 col-md-4 col-lg-7 rounded'>");
                 // adding click power to dynamically generated buttons
-                searchHistoryDiv.click(function(event) {
+                searchHistoryDiv.click(function (event) {
                     event.preventDefault();
                     console.log(event);
                 });
